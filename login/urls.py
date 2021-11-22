e2f8804cefed00ccd60938f2cb0c2b3560355b86
minhmannh2001 <nguyenminhmannh2001@gmail.com>
2021-11-23 01:55:05 +0700

    signin, signup page done

--- login/urls.py
+++ login/urls.py
@@ -1,4 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
+
+app_name = 'login'
 
 urlpatterns = [
+    path('signin/', views.sign_in, name='signin'),
+    path('signup/', views.sign_up, name='signup'),
 ]
--- login/views.py
+++ login/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def sign_in(request):
+    return render(request, 'sign_in.html')
+
+
+def sign_up(request):
+    return render(request, 'sign_up.html')
